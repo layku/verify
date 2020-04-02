a7a8443347648174e8fa9f8132b8af50bebaafcd
懒人董 <dongdingzhuo@gmail.com>
2020-04-02 20:01:36 +0800

    修改BUG

--- rest/src/main/java/cn/layku/verify/rest/controller/auth/UserController.java
+++ rest/src/main/java/cn/layku/verify/rest/controller/auth/UserController.java
@@ -6,7 +6,6 @@ import cn.layku.verify.rest.service.auth.UserService;
 import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
